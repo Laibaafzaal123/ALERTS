1deb54a42e7b1ff7c77abc38bf8cc131f132ad02
Laibaafzaal123 <laibafzaal779@gmail.com>
2025-07-03 15:44:17 +0500

    first commit

---
+++ alert.js
@@ -0,0 +1,30 @@
+// Q1: Write a script to greet your website visitor using JS alert box.
+alert("Welcome to our Website!");
+
+// Q2: Write a script to display following message on your web page:
+// "Error! Please enter a valid password."
+alert("Error! Please enter a valid password.");
+
+// Q3: Write a script to display following message on your web page (Hint: Use line break):
+// "Welcome to JS Land...
+// Happy Coding!"
+alert("Welcome to JS Land... \n Happy Coding!");
+
+// Q4: Write a script to display following messages in sequence:
+// "Welcome to JS Land..."
+// "Happy Coding!"
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+//Note: When multiple alerts are used in sequence, browsers like Chrome may show a checkbox:
+// 
+//Q5. Generate the following message through browser’s developer console:
+console.log("Hello... I can run JS through my web browser’s console");
+
+//Q6 Make use of alerts in your new/existing HTML & CSS project.
+alert("Welcome to My Portfolio Website!");
+
+//Q7: Practice placement of <script> element in following sections:
+// This work is done on html page 
+
+
+
